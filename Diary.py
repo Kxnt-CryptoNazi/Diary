d1cbcfe5e103cc1921eda84678dde98d7d26c24d
Kezrah <eldron201111@gmail.com>
2026-05-12 20:28:19 +0800

    commit

--- Diary.py
+++ Diary.py
@@ -12,3 +12,13 @@ def add_entry():
         f.write(entry + "\n")
     print("Entry added!")
 
+def read_diary():
+    try:
+        with open("Diary.txt", "r") as f:
+            lines = f.readlines()
+            print("\n--- Diary Content ---")
+            for line in lines:
+                print(line.strip())
+            print("Total entries:", len(lines) - 1)
+    except FileNotFoundError:
+        print("Diary not found!")
